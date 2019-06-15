8525986c0691bedac5be79ad857e13c9b9b18069
OTITOOLA Aanuoluwapo Ayodeji [EPN-Cognitive Technology] <AOTITOOLA@ecobank.group>
2019-06-15 17:48:27 +0100

    initial commit

---
+++ src/test/java/com/learn/jokesApp/ChuckNorrisJokesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learn.jokesApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChuckNorrisJokesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
